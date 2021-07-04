71bcf615d3aa66e1f5de37992dcdd2033168a82b
Leon Hsu <leonhsu95@gmail.com>
2021-07-04 23:13:56 +1000

    Middleware error

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -2,16 +2,18 @@ const path = require("path");
 const router = require("express").Router();
 
 
-  router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname+ "/index.html"));
+module.exports = function (app) {
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname+ "../public/index.html"));
   });
 
-  router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname+ "/exercise.html"));
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname+ "../public/exercise.html"));
   });
 
-  router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname+ "/stats.html"));
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname+ "../public/stats.html"));
   });
+};
 
-moudle.exports = router
+module.exports = router;
